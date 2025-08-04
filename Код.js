37f8fa7fda5f482c25cb33cb473f8746b7f4bb9c
Evgeny <eklimov84@gmail.com>
2025-08-04 21:33:06 +0300

    Обновлен идентификатор TelegramApiLib и добавлена тестовая функция

--- Код.js
+++ Код.js
@@ -1,2 +1,11 @@
 // Этот файл оставлен пустым, так как весь код был реорганизован в модули.
 // См. файлы с префиксами 1_, 2_, 3_ и т.д.
+
+function testTelegramApiLib() {
+  try {
+    const botInfo = TelegramApiLib.getMe();
+    Logger.log("Информация о боте: " + JSON.stringify(botInfo));
+  } catch (e) {
+    Logger.log("Ошибка при тестировании TelegramApiLib: " + e.message);
+  }
+}
